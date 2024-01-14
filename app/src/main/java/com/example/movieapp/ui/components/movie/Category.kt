1b09d4e4471962bc272ddee423a43682af186663
Jerry <jylee@maxst.com>
2024-01-14 16:11:55 +0900

    Custom Button 만들기

--- app/src/main/java/com/example/movieapp/ui/components/movie/Category.kt
+++ app/src/main/java/com/example/movieapp/ui/components/movie/Category.kt
@@ -19,11 +19,14 @@ fun CategoryRow() {
         CategoryTitle("Action")
         LazyRow(
             contentPadding = PaddingValues(
-                horizontal = 10.dp
+                horizontal = Paddings.large
             )
         ) {
 //            itemsIndexed
             item {
+                MovieItem()
+                MovieItem()
+                MovieItem()
                 MovieItem()
             }
         }
--- app/src/main/java/com/example/movieapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/movieapp/ui/theme/Theme.kt
@@ -6,8 +6,10 @@ import androidx.compose.material.Shapes
 import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.staticCompositionLocalOf
 import com.example.movieapp.ui.theme.color.ColorSet
+import com.example.movieapp.ui.theme.color.MyColors
 
 private val LocalColors = staticCompositionLocalOf { ColorSet.Red.LightColors }
 @Composable
@@ -33,3 +35,8 @@ fun MovieAppTheme(
         )
     }
 }
+
+val MaterialTheme.colorScheme : MyColors
+@Composable
+@ReadOnlyComposable
+get() = LocalColors.current
